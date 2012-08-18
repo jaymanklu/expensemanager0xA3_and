4798945ddfb36ec13bef99cb830dac4225b9eb69
jaymanklu <jayuser@git>
2012-08-18 15:18:20 +0530

    first

---
+++ gen/noncom/jay/xA3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package noncom.jay.xA3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/noncom/jay/xA3/HomeActivity.java
@@ -0,0 +1,21 @@
+package noncom.jay.xA3;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
