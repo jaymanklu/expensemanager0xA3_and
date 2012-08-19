e5830ddc76686dadc20b507d6ca1b69bde19f0dd
jaymanklu <jayuser@git>
2012-08-19 15:06:03 +0530

    19thAug

--- src/noncom/jay/xA3/HomeActivity.java
+++ src/noncom/jay/xA3/HomeActivity.java
@@ -10,7 +10,7 @@ public class HomeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.Main);
     }
 
     @Override
