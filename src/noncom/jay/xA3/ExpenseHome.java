abb9c7334ee40b06d4bac15e00cfd610f24b1c39
Jayanth Manklu <jaymanklu@github123.com>
2013-01-16 08:21:09 +0530

    0820hrs 16Jan2013

--- src/noncom/jay/xA3/ExpenseHome.java
+++ src/noncom/jay/xA3/ExpenseHome.java
@@ -1,10 +1,14 @@
 package noncom.jay.xA3;
 
 
+
+
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class ExpenseHome extends Activity {
 
@@ -12,6 +16,15 @@ public class ExpenseHome extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button salBt = (Button)findViewById(R.id.Salary);
+        salBt.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(ExpenseHome.this, SalaryActivity.class);
+	             startActivity(intent);
+				
+			}
+		});
         
         
 
